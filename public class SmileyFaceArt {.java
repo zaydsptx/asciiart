7a46ca5c1b9b87b744f658c5637e3576d2a36e82
zaydsptx <123398095+zaydsptx@users.noreply.github.com>
2023-09-29 00:07:43 -0500

    Add files via upload

---
+++ public class SmileyFaceArt {.java
@@ -0,0 +1,17 @@
+public class SmileyFaceArt {
+    public static void main(String[] args) {
+        // Smiley face
+        System.out.println("     .-\"\"\"\"\"\"-.");
+        System.out.println("   .'           '.");
+        System.out.println("  /   O       O   \\");
+        System.out.println(" :                 :");
+        System.out.println(" |                 |");
+        System.out.println(" :   .-------.   :");
+        System.out.println("  \\  '- - - -'  /");
+        System.out.println("   '. ------- .'");
+        System.out.println("     '-.....-'");
+        
+        // Text "CSA"
+        System.out.println("CSA");
+    }
+}
